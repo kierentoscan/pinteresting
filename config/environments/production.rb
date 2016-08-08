0c3f56e6ee7fb12a987e62d76acba8ec8ab2c148
Kieren Toscan <kieren.toscan@gmail.com>
2016-08-08 19:36:34 +0100

    Added Devise Gem; log in and sign out routes and views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    #required for heroku
+    #NOTE THIS SET TO PRODUCTION NAME
+  config.action_mailer.default_url_options = { host: 'omr-kjt-pinteresting.herokuapp.com' }
 end
